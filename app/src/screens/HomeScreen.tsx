ca051eeca208a9486eb3cd38d756507414677166
Fabio-Ricci <fabioricci9@gmail.com>
2022-09-13 16:32:04 -0300

    [app] added absolute paths

---
+++ app/src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import Widget from '@app/components/Widget';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Widget></Widget>
+    </View>
+  );
+};
+
+export default HomeScreen;
