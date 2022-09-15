d6928fbe1744d0a2b1cfd8507966408c5c90b42b
Erick Saito <saitoerick@gmail.com>
2022-09-15 16:40:29 -0300

    create open weather api as class

--- server/src/open-weather/api.ts
+++ server/src/open-weather/api.ts
@@ -1,22 +1,21 @@
-import Axios from 'axios';
+import { Injectable } from '@nestjs/common';
+import { Axios } from 'axios';
 import { OPEN_WEATHER_API_KEY, OPEN_WEATHER_URL } from '../config/config';
 import { OpenWeatherRequest, OpenWeatherResponse } from './types';
 
-const OpenWeatherAPI = ({ axios = Axios } = {}) => {
-  const api = { getWeather };
+@Injectable()
+export class OpenWeatherAPI {
+  private readonly axios: Axios;
 
-  async function getWeather(
-    req: OpenWeatherRequest,
-  ): Promise<OpenWeatherResponse> {
-    const { latitude, longitude, exclude } = req;
-    const { data } = await axios.get(
-      `${OPEN_WEATHER_URL}?appid=${OPEN_WEATHER_API_KEY}&units=metric&lat=${latitude}&lon=${longitude}&exclude=${exclude.toString()}`,
-    );
-
-    return data;
+  constructor() {
+    this.axios = new Axios();
   }
 
-  return api;
-};
-
-export default OpenWeatherAPI;
+  async getWeather(req: OpenWeatherRequest): Promise<OpenWeatherResponse> {
+    const { latitude, longitude, exclude } = req;
+    const { data } = await this.axios.get(
+      `${OPEN_WEATHER_URL}?appid=${OPEN_WEATHER_API_KEY}&units=metric&lat=${latitude}&lon=${longitude}&exclude=${exclude.toString()}`,
+    );
+    return data;
+  }
+}
