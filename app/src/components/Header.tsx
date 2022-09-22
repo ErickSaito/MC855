6c6a5f7fae97afd1e9cbce91dc71ac1e9b5e3ea4
Fabio-Ricci <fabioricci9@gmail.com>
2022-09-21 21:05:13 -0300

    minor fix

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -3,12 +3,12 @@ import { Text, View } from 'react-native';
 
 const Header = () => {
   return (
-    <View className="items-center justify-between flex-row px-5 android:mt-4">
-      <Text className="text-white font-zenMaru">Settings</Text>
+    <View className="items-center justify-between flex-row px-5 android:top-9">
+      <Text className="text-white font-medium font-zenMaru">Settings</Text>
       <Text className="font-black text-xl text-white font-zenMaru">
         umbrella?
       </Text>
-      <Text className="text-white font-zenMaru">Notifications</Text>
+      <Text className="text-white font-medium font-zenMaru">Notifications</Text>
     </View>
   );
 };
