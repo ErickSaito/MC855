4d6c76077145e8f3409aa27553d5491987b0d9aa
Erick Saito <saitoerick@gmail.com>
2022-10-04 00:07:06 -0300

    add get  message by id

--- server/src/message/repository.ts
+++ server/src/message/repository.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { GenericRepository } from '../generics/GenericRepository';
+import { MESSAGES } from './data';
 import { WeatherMessage } from './types';
 
 @Injectable()
 export class WeatherMessageRepository
   implements GenericRepository<WeatherMessage>
 {
-  get(id: string): Promise<WeatherMessage> {
-    return undefined;
+  async get(id: string): Promise<WeatherMessage> {
+    const msgPromise = new Promise<WeatherMessage>((resolve, reject) => {
+      const messages = MESSAGES.filter((mgs) => mgs.id === id);
+      if (messages.length) {
+        resolve(messages[0]);
+      }
+      reject(undefined);
+    });
+
+    return await msgPromise;
   }
 
   filter(data: object): Promise<WeatherMessage[]> {}
