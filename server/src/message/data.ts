5f51df9f254d33fa8302135e87db982ee9ec935a
Erick Saito <saitoerick@gmail.com>
2022-10-11 17:39:04 -0300

    refactor weather messages

--- server/src/message/data.ts
+++ server/src/message/data.ts
@@ -5,26 +5,26 @@ export const MESSAGES: WeatherMessage[] = [
     id: '1',
     rain_intensity: 'intense',
     message:
-      'Did you rent a boat? Maybe could be a good idea, a river will fall from the sky {when}',
+      'Did you rent a boat? Maybe could be a good idea, a river will fall from the sky today',
   },
   {
     id: '2',
     rain_intensity: 'high',
-    message: 'A raincoat may be needed. {when} will rain a lot',
+    message: 'A raincoat may be needed. It will rain a lot',
   },
   {
     id: '3',
     rain_intensity: 'normal',
-    message: 'You should get an umbrella, it will rain {when}',
+    message: 'You should get an umbrella, it will rain, but not too much',
   },
   {
     id: '4',
     rain_intensity: 'low',
-    message: 'It will just drizzling {when}, umbrella is not needed',
+    message: 'It will just drizzling today, i think an umbrella is not needed',
   },
   {
     id: '5',
     rain_intensity: 'none',
-    message: 'The sun will shine {when}',
+    message: 'Have a wonrderful day, It will not rain today... i hope so',
   },
 ];
