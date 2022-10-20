4328c8d34faeffab307a9411185fb030814bd620
paulopacitti <ppacitti@outlook.com>
2022-10-20 13:30:14 -0300

    [app][fix] Remove package-lock.json and unnecessary comments

--- app/src/services/device.service.ts
+++ app/src/services/device.service.ts
@@ -3,12 +3,6 @@ import { SyncDeviceDTO } from '../types/device';
 
 export default class DeviceService {
   static async sync(payload: SyncDeviceDTO) {
-    /* TODO: Sync Device Token
-     * - https://rnfirebase.io/messaging/server-integration#device-tokens
-     * - https://day.js.org/ - Short and up to date time library, API-compatible with moment.js
-     * avoid using moment.js since it's not being maitained for years and it's very large too.
-     */
-
     try {
       const res = await api.post('/device/sync', payload);
       return res.data;
