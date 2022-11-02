369dc1680167db244a6457b52397dea8449ebcae
paulopacitti <ppacitti@outlook.com>
2022-11-02 15:39:21 -0300

    [server] Add test and scheduled notification routines for push notifications

--- server/src/device/service.ts
+++ server/src/device/service.ts
@@ -1,9 +1,75 @@
 import { Injectable } from '@nestjs/common';
 import { SyncDeviceDTO } from './types';
-import { db } from '../config/firebase';
+import { db, msg } from '../config/firebase';
+import { schedule } from 'node-cron';
 
 @Injectable()
 export class DeviceService {
+  constructor() {
+    this.scheduleNotification();
+  }
+
+  async scheduleNotification() {
+    // Sends notification everyday,  8am GMT-3
+    schedule('0 11 * * *', async () => {
+      const query = await db.collection('devices').get();
+      const tokens = query.docs.map((doc) => doc.id);
+      console.log(tokens);
+      msg
+        .sendMulticast({
+          tokens,
+          notification: {
+            title: 'Umbrella',
+            body: 'A new weather report for you :)',
+          },
+        })
+        .then((response) => {
+          if (response.failureCount > 0) {
+            const failedTokens = [];
+            response.responses.forEach((resp, i) => {
+              if (!resp.success) {
+                failedTokens.push(tokens[i]);
+              }
+            });
+            this.cleanTokens(failedTokens);
+          }
+        })
+        .catch((error) => console.log(error));
+    });
+  }
+
+  async testNotification() {
+    const query = await db.collection('devices').get();
+    const tokens = query.docs.map((doc) => doc.id);
+    console.log(tokens);
+    msg
+      .sendMulticast({
+        tokens,
+        notification: {
+          title: 'Umbrella',
+          body: 'Test',
+        },
+      })
+      .then((response) => {
+        if (response.failureCount > 0) {
+          const failedTokens = [];
+          response.responses.forEach((resp, i) => {
+            if (!resp.success) {
+              failedTokens.push(tokens[i]);
+            }
+          });
+          this.cleanTokens(failedTokens);
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+
+  cleanTokens(tokens: Array<string>) {
+    tokens.forEach((token) => {
+      db.collection('devices').doc(token).delete();
+    });
+  }
+
   async sync(req: SyncDeviceDTO): Promise<boolean> {
     try {
       await db.collection('devices').doc(req.token).set(req);
