6ccca24b285a9cd934a56fd351397e3091a59c89
paulopacitti <ppacitti@outlook.com>
2022-11-02 12:37:37 -0300

    [server] Setup Firebase messaging config

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -1,3 +1,14 @@
+import fs from 'fs';
+
 export const OPEN_WEATHER_URL = process.env.OPEN_WEATHER_URL;
 export const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
-export const GOOGLE_APPLICATION_CREDENTIALS = process.env.FIREBASE_CONFIG;
+export const GOOGLE_APPLICATION_CREDENTIALS =
+  process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
+fs.writeFile(
+  'mc855-umbrella-firebase-adminsdk.json',
+  GOOGLE_APPLICATION_CREDENTIALS,
+  (error) => {
+    if (error) console.log(error);
+  },
+);
--- server/src/config/firebase.ts
+++ server/src/config/firebase.ts
@@ -1,7 +1,11 @@
-import { applicationDefault, initializeApp } from 'firebase-admin/app';
-import { firestore } from 'firebase-admin';
+import { initializeApp } from 'firebase-admin/app';
+import { credential, firestore, messaging } from 'firebase-admin';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const serviceAccount = require('./mc855-umbrella-firebase-adminsdk.json');
 
 export const app = initializeApp({
-  credential: applicationDefault(),
+  credential: credential.cert(serviceAccount),
 });
 export const db = firestore();
+export const msg = messaging();
